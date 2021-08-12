1cef4066aa91323f84484a2392d852c5f8b6202d
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-12 08:38:43 -0400

    Create Define.js

---
+++ Define.js
@@ -0,0 +1,9 @@
+function execute() {
+const Test = document.getElementById("Test");
+Test.innerText = toolbox.JS[0].execute.Name;
+Test.style.backgroundColor = toolbox.JS[0].execute.color;
+var input = document.getElementById("INPUT");
+function executeclicked(Value) {
+alert(Value);
+}
+}
